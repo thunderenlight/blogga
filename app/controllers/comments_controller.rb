c3326801480863ccde0715160788f726dc7f4322
thunderenlight <thunderenlight@gmail.com>
2020-02-06 20:10:51 -0600

    added paperclip and fixed comment destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+	before_action :get_article, only: [:create, :destroy]
+
 	def create
 		@comment = Comment.new(comment_params)
 		@comment.article_id = params[:article_id]
@@ -11,12 +13,17 @@ class CommentsController < ApplicationController
 
 
 	def destroy
-		@comment = Comment.destroy(params[:id])
-		@comment.article_id = params[:article_id]
-		flash.notice = "Article by '#{@comment.author_name}' deleted!"
+		#@comment.author_name = @article.comments(params[:author_name])
+		@comment = @article.comments.destroy(params[:id])
+		flash.notice = "Comment deleted!"
 
-		redirect_to article_path
+		redirect_to @article
 	end
+
+	def get_article
+		@article = Article.find(params[:article_id])
+	end
+
 	def comment_params
 		params.require(:comment).permit(:author_name, :body)
 	end
